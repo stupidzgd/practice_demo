f7ed1c13b96b60cfd5a4ec3dde490b298f88c8ea
stupidzgd <13426313932@163.com>
2018-06-28 13:49:08 +0800

    更新jsonp

--- ajax-jsonp-demo/ajax.js
+++ ajax-jsonp-demo/ajax.js
@@ -1,64 +1,68 @@
 var myAjax = {
-    jsonpAjax: function (param) {
-        if (param.dataType && param.dataType ==='jsonp') {
-            var isSameOrigin = true;
-
-            //每次请求带上时间戳
-            var callback = 'Jsonp' + new Date().getTime();
-            var myUrl = window.location.href;
-            var targetUrl = param.url;
-            var myProtocol = myUrl.split('://')[0];
-            var myHost = window.location.host;
-            var targetProtocol = targetUrl.split('://')[0];
-            var targetHost = targetUrl.split('://')[1];
-            if(myProtocol === targetProtocol && myHost === targetHost) {
-                isSameOrigin = true;
-            } else {
-                isSameOrigin = false;
-            }
-            if (isSameOrigin) {
-                this.ajax(param.type, param.url, param.success, param.data, true);
-            } else {
-                window[callback] = param.success;
-                var oScript = document.createElement('script');
-                //判断传没传jsonp这个参数，如果有请求查询参数用这个值，如果没有默认用callback
-                var src = param.url + '?' + param.data + '&' + (param.jsonp || 'callback') + '=' + callback;
-                oScript.src = src;
-                document.body.appendChild(oScript);
-                document.body.removeChild(oScript);
-            }
-        } else {
-            this.ajax(param.type, param.url, param.success, param.data, true);
-        }
-    },
-
-    ajax: function (method, url, callback, data, flag) {
-        var xhr = null;
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else {
-            xhr = new window.ActiveObject('Microsoft.XMLHTTP');
-        }
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState == 4) {
-                if (xhr.status == 200) {
-                    callback(xhr.responseText);
-                }
-            }
-        }
-        method = method.toUpperCase();
-        if (method === 'GET') {
-            var time = new Date().getTime();
-            xhr.open(method, url + '?' + data + '?timer=' + time, flag);
-            xhr.send();
-        } else if (method === 'POST') {
-            xhr.open(method, url, flag);
-            xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
-            xhr.send(data);
-        }
+  jsonp: function (params) {
+    if (params.dataType === 'jsonp') {
+      //每次请求带上时间戳
+      var callback = 'Jsonp' + new Date().getTime();
+      var isSameOrigin = this.hasSameOrigin(params.url);
+      if (!isSameOrigin) {
+        window[callback] = params.success;
+        //判断传没传jsonp这个参数，如果有请求查询参数用这个值，如果没有默认用callback
+        var src = params.url + '?' + params.data + '&' + (params.jsonp || 'callback') + '=' + callback;
+        var oScript = document.createElement('script');
+        oScript.src = src;
+        document.body.appendChild(oScript);
+        document.body.removeChild(oScript);
+      } else {
+        this.ajax(params.method, params.url, params.success, params.data, true);
+      }
+    } else {
+      this.ajax(params.method, params.url, params.success, params.data, true);
     }
+  },
+
+  hasSameOrigin: function (url) {
+    var hostUrl = window.location.href;
+    var myProtocol = hostUrl.split('://')[0];
+    var myPort = window.location.port;
+    var myHost = window.location.host;
+    if (myPort) {
+      myHost = window.location.host.split(':')[0];
+    }
+    var targetProtocol = url.split('://')[0];
+    var targetHost = url.split('://')[1].split('/')[0];
+    var targetPort = '';
+    if (targetHost.indexOf(':') != -1) {
+      targetPort = targetHost.split(':')[1];
+      targetHost = targetHost.split(':')[0];
+    }
+    if (targetHost === myHost && targetProtocol === myProtocol && targetPort === targetPort) {
+      return true;
+    } else {
+      return false;
+    }
+  },
+
+  ajax: function (method, url, callback, data, flag) {
+    var xhr = null;
+    if (window.XMLHttpRequest) {
+      xhr = new XMLHttpRequest();
+    } else {
+      xhr = new ActiveXObject('Microsoft.XMLHTTP');
+    }
+    xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+        callback(xhr.responseText);
+      }
+    }
+    method = method.toUpperCase();
+    if (method == 'POST') {
+      xhr.open(method, url, flag);
+      xhr.setRequestHeader('Content-type', 'application/x-www-form-urlencoded');
+      xhr.send(data);
+    } else if (method == 'GET') {
+      var timer = new Date().getTime();
+      xhr.open(method, url + '?' + data + '&timer=' + timer);
+      xhr.send();
+    }
+  }
 }
-
-
-
-
