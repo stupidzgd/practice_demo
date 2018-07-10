3cd0aed121dd2106c8834c2401e0955678e21755
stupidzgd <stupidzgd@163.com>
2018-07-10 23:44:30 +0800

    简单排序

--- 排序_去重/demo.js
+++ 排序_去重/demo.js
@@ -95,3 +95,35 @@ function quick(arr) {
   }
   return quick(left).concat(base, quick(right));
 }
+
+function quickSort2(arr, start, end) {
+  if (start > end) {
+    return;
+  }
+  let i = start,
+    j = end,
+    pivot = arr[start]; //存放基准数
+  while (i !== j) {
+    // 从右边开始，找第一个小于基准的位置
+    while (arr[j] >= pivot && i < j) {
+      j--;
+    }
+    // 从左边开始，找第一个大于基准的位置
+    while (arr[i] <= pivot && i < j) {
+      i++
+    }
+    // 交换两个数
+    if (i < j) {
+      let tmp = arr[i];
+      arr[i] = arr[j];
+      arr[j] = tmp;
+    }
+  }
+  // 最后把基准数归位
+  arr[start] = arr[i];
+  arr[i] = pivot;
+  // 递归处理左边
+  quickSort(arr, start, i - 1);
+  // 递归处理右边
+  quickSort(arr, i + 1, end);
+}
